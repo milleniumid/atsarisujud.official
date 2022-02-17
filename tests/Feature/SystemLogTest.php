c8ad4c10b5bb76685e40b9c3f4359ffe30545a4b
Rezky Maulana <rezkymaulanaa06@gmail.com>
2022-02-17 14:56:43 +0700

    fix first testing

--- tests/Feature/SystemLogTest.php
+++ tests/Feature/SystemLogTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class SystemLogTest extends TestCase
 {
@@ -15,6 +16,15 @@ class SystemLogTest extends TestCase
      */
     public function test_system_log_page_can_be_rendered()
     {
+        $user = User::factory()->create();
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+
         $response = $this->get('/log/system');
 
         $response->assertStatus(200);
