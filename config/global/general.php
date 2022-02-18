2d67dc8909182884527aec0e5b8702b8b1d1e52d
Rezky Maulana <rezkymaulanaa06@gmail.com>
2022-02-18 19:40:25 +0700

    update general/global

--- config/global/general.php
+++ config/global/general.php
@@ -2,198 +2,16 @@
 return array(
     // Product
     'product' => array(
-        'name'        => 'Metronic',
-        'description' => 'Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme',
+        'name'        => 'Atsari Sujud',
+        'description' => 'Atsa',
         'preview'     => 'https://preview.keenthemes.com/metronic8/laravel',
         'home'        => 'https://keenthemes.com/metronic',
         'purchase'    => 'https://1.envato.market/EA4JP',
         'licenses'    => array(
             'terms' => 'https://themeforest.net/licenses/standard',
-            'types' => array(
-                array(
-                    'title'       => 'Regular License',
-                    'description' => 'For single end product used by you or one client',
-                    'tooltip'     => 'Use, by you or one client in a single end product which end users are not charged for',
-                    'price'       => '39',
-                ),
-                array(
-                    'title'       => 'Extended License',
-                    'description' => 'For single SaaS app with paying users',
-                    'tooltip'     => 'Use, by you or one client, in a single end product which end users can be charged for.',
-                    'price'       => '939',
-                ),
-            ),
+            'types' => array(),
         ),
-        'demos'       => array(
-            'demo1' => array(
-                'title'       => 'Demo 1',
-                'description' => 'Default Dashboard',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo1.png',
-            ),
-
-            'demo2' => array(
-                'title'       => 'Demo 2',
-                'description' => 'SaaS Application',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo2.png',
-            ),
-
-            'demo3' => array(
-                'title'       => 'Demo 3',
-                'description' => 'New Trend',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo3.png',
-            ),
-
-            'demo4' => array(
-                'title'       => 'Demo 4',
-                'description' => 'Intranet Application',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo4.png',
-            ),
-
-            'demo5' => array(
-                'title'       => 'Demo 5',
-                'description' => 'Support Forum',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo5.png',
-            ),
-
-            'demo6' => array(
-                'title'       => 'Demo 6',
-                'description' => 'Admin Backend',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo6.png',
-            ),
-
-            'demo7' => array(
-                'title'       => 'Demo 7',
-                'description' => 'CRM Dashboard',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo7.png',
-            ),
-
-            'demo8' => array(
-                'title'       => 'Demo 8',
-                'description' => 'Core Dashboard',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo8.png',
-            ),
-
-            'demo9' => array(
-                'title'       => 'Demo 9',
-                'description' => 'Fancy Dashboard',
-                'published'   => true,
-                'thumbnail'   => 'demos/demo9.png',
-            ),
-
-            'demo10' => array(
-                'title'       => 'Demo 10',
-                'description' => 'Project Grid',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo10.png',
-            ),
-
-            'demo11' => array(
-                'title'       => 'Demo 11',
-                'description' => 'Light Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo11.png',
-            ),
-
-            'demo12' => array(
-                'title'       => 'Demo 12',
-                'description' => 'Creative Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo12.png',
-            ),
-
-            'demo13' => array(
-                'title'       => 'Demo 13',
-                'description' => 'Classic Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo13.png',
-            ),
-
-            'demo14' => array(
-                'title'       => 'Demo 14',
-                'description' => 'Interactive Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo14.png',
-            ),
-
-            'demo15' => array(
-                'title'       => 'Demo 15',
-                'description' => 'Member Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo15.png',
-            ),
-
-            'demo16' => array(
-                'title'       => 'Demo 16',
-                'description' => 'Modern Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo16.png',
-            ),
-
-            'demo17' => array(
-                'title'       => 'Demo 17',
-                'description' => 'Fancy Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo17.png',
-            ),
-
-            'demo18' => array(
-                'title'       => 'Demo 18',
-                'description' => 'Minimalistic Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo18.png',
-            ),
-
-            'demo19' => array(
-                'title'       => 'Demo 19',
-                'description' => 'Futuristic Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo19.png',
-            ),
-
-            'demo20' => array(
-                'title'       => 'Demo 20',
-                'description' => 'Advanced Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo20.png',
-            ),
-
-            'demo21' => array(
-                'title'       => 'Demo 21',
-                'description' => 'Monochrome Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo21.png',
-            ),
-
-            'demo22' => array(
-                'title'       => 'Demo 22',
-                'description' => 'Social Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo22.png',
-            ),
-
-            'demo23' => array(
-                'title'       => 'Demo 23',
-                'description' => 'SaaS Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo23.png',
-            ),
-
-            'demo24' => array(
-                'title'       => 'Demo 24',
-                'description' => 'Compact Dashboard',
-                'published'   => false,
-                'thumbnail'   => 'demos/demo24.png',
-            ),
-
-        ),
+        'demos'       => array(),
     ),
 
     // Meta
@@ -254,19 +72,19 @@ return array(
         // Engage
         'engage'        => array(
             'demos'    => array(
-                'enabled'   => true,
+                'enabled'   => false,
                 'direction' => 'end',
             ),
             'explore'  => array(
-                'enabled'   => true,
+                'enabled'   => false,
                 'direction' => 'end',
             ),
             'help'     => array(
-                'enabled'   => true,
+                'enabled'   => false,
                 'direction' => 'end',
             ),
             'purchase' => array(
-                'enabled' => true,
+                'enabled' => false,
             ),
         ),
     ),
