55bb48e92d2e51ac5ec2106f7a0b2d014598fd6e
Rezky Maulana <rezkymaulanaa06@gmail.com>
2022-02-19 18:24:05 +0700

    fix base/base blade

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"{!! theme()->printHtmlAttributes('html') !!} {{ theme()->printHtmlClasses('html') }}>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {!! theme()->printHtmlAttributes('html') !!}
+    {{ theme()->printHtmlClasses('html') }}>
 {{-- begin::Head --}}
+
 <head>
-    <meta charset="utf-8"/>
-    <title>{{ ucfirst(theme()->getOption('meta', 'title')) }} | Keenthemes</title>
-    <meta name="description" content="{{ ucfirst(theme()->getOption('meta', 'description')) }}"/>
-    <meta name="keywords" content="{{ theme()->getOption('meta', 'keywords') }}"/>
-    <link rel="canonical" href="{{ ucfirst(theme()->getOption('meta', 'canonical')) }}"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <link rel="shortcut icon" href="{{ asset(theme()->getDemo() . '/' .theme()->getOption('assets', 'favicon')) }}"/>
+    <meta charset="utf-8" />
+    <title>{{ __(empty(theme()->getOption('pages', 'title')) ? env('APP_NAME') : theme()->getOption('pages', 'title')) }}</title>
+    <meta name="description" content="{{ ucfirst(theme()->getOption('meta', 'description')) }}" />
+    <meta name="keywords" content="{{ theme()->getOption('meta', 'keywords') }}" />
+    <link rel="canonical" href="{{ ucfirst(theme()->getOption('meta', 'canonical')) }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="{{ asset(theme()->getDemo() . '/' . theme()->getOption('assets', 'favicon')) }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{-- begin::Fonts --}}
@@ -37,7 +39,7 @@
             @if (strpos($file, 'plugins') !== false)
                 {!! preloadCss(assetCustom($file)) !!}
             @else
-                <link href="{{ assetCustom($file) }}" rel="stylesheet" type="text/css"/>
+                <link href="{{ assetCustom($file) }}" rel="stylesheet" type="text/css" />
             @endif
         @endforeach
         {{-- end::Global Stylesheets Bundle --}}
@@ -53,45 +55,47 @@
 {{-- end::Head --}}
 
 {{-- begin::Body --}}
+
 <body {!! theme()->printHtmlAttributes('body') !!} {!! theme()->printHtmlClasses('body') !!} {!! theme()->printCssVariables('body') !!}>
 
-@if (theme()->getOption('layout', 'loader/display') === true)
-    {{ theme()->getView('layout/_loader') }}
-@endif
+    @if (theme()->getOption('layout', 'loader/display') === true)
+        {{ theme()->getView('layout/_loader') }}
+    @endif
 
-@yield('content')
+    @yield('content')
 
-{{-- begin::Javascript --}}
-@if (theme()->hasOption('assets', 'js'))
-    {{-- begin::Global Javascript Bundle(used by all pages) --}}
-    @foreach (array_unique(theme()->getOption('assets', 'js')) as $file)
-        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
-    @endforeach
-    {{-- end::Global Javascript Bundle --}}
-@endif
+    {{-- begin::Javascript --}}
+    @if (theme()->hasOption('assets', 'js'))
+        {{-- begin::Global Javascript Bundle(used by all pages) --}}
+        @foreach (array_unique(theme()->getOption('assets', 'js')) as $file)
+            <script src="{{ asset(theme()->getDemo() . '/' . $file) }}"></script>
+        @endforeach
+        {{-- end::Global Javascript Bundle --}}
+    @endif
 
-@if (theme()->hasOption('page', 'assets/vendors/js'))
-    {{-- begin::Page Vendors Javascript(used by this page) --}}
-    @foreach (array_unique(theme()->getOption('page', 'assets/vendors/js')) as $file)
-        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
-    @endforeach
-    {{-- end::Page Vendors Javascript --}}
-@endif
+    @if (theme()->hasOption('page', 'assets/vendors/js'))
+        {{-- begin::Page Vendors Javascript(used by this page) --}}
+        @foreach (array_unique(theme()->getOption('page', 'assets/vendors/js')) as $file)
+            <script src="{{ asset(theme()->getDemo() . '/' . $file) }}"></script>
+        @endforeach
+        {{-- end::Page Vendors Javascript --}}
+    @endif
 
-@if (theme()->hasOption('page', 'assets/custom/js'))
-    {{-- begin::Page Custom Javascript(used by this page) --}}
-    @foreach (array_unique(theme()->getOption('page', 'assets/custom/js')) as $file)
-        <script src="{{ asset(theme()->getDemo() . '/' .$file) }}"></script>
-    @endforeach
-    {{-- end::Page Custom Javascript --}}
-@endif
-{{-- end::Javascript --}}
+    @if (theme()->hasOption('page', 'assets/custom/js'))
+        {{-- begin::Page Custom Javascript(used by this page) --}}
+        @foreach (array_unique(theme()->getOption('page', 'assets/custom/js')) as $file)
+            <script src="{{ asset(theme()->getDemo() . '/' . $file) }}"></script>
+        @endforeach
+        {{-- end::Page Custom Javascript --}}
+    @endif
+    {{-- end::Javascript --}}
 
-@if (theme()->getViewMode() === 'preview')
-    {{ theme()->getView('partials/trackers/_ga-tag-manager-for-body') }}
-@endif
+    @if (theme()->getViewMode() === 'preview')
+        {{ theme()->getView('partials/trackers/_ga-tag-manager-for-body') }}
+    @endif
 
-@yield('scripts')
+    @yield('scripts')
 </body>
 {{-- end::Body --}}
+
 </html>
