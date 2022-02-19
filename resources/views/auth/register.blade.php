0a64f2a26661fdc0783be50ae2260d496848ebb5
Rezky Maulana <rezkymaulanaa06@gmail.com>
2022-02-19 18:23:12 +0700

    fix login and register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,11 @@
 <x-auth-layout>
 
     <!--begin::Signup Form-->
-    <form method="POST" action="{{ theme()->getPageUrl('register') }}" class="form w-100" novalidate="novalidate" id="kt_sign_up_form">
-    @csrf
+    <form method="POST" action="{{ theme()->getPageUrl('register') }}" class="form w-100" novalidate="novalidate"
+        id="kt_sign_up_form">
+        @csrf
 
-    <!--begin::Heading-->
+        <!--begin::Heading-->
         <div class="text-center mb-10">
             <!--begin::Title-->
             <h1 class="text-dark mb-3">
@@ -29,14 +30,16 @@
             <!--begin::Col-->
             <div class="col-xl-6">
                 <label class="form-label fw-bolder text-dark fs-6">{{ __('Nama Depan') }}</label>
-                <input class="form-control form-control-lg form-control-solid" type="text" name="first_name" autocomplete="off" value="{{ old('first_name') }}"/>
+                <input class="form-control form-control-lg form-control-solid" type="text" name="first_name"
+                    autocomplete="off" value="{{ old('first_name') }}" />
             </div>
             <!--end::Col-->
 
             <!--begin::Col-->
             <div class="col-xl-6">
                 <label class="form-label fw-bolder text-dark fs-6">{{ __('Nama Belakang') }}</label>
-                <input class="form-control form-control-lg form-control-solid" type="text" name="last_name" autocomplete="off" value="{{ old('last_name') }}"/>
+                <input class="form-control form-control-lg form-control-solid" type="text" name="last_name"
+                    autocomplete="off" value="{{ old('last_name') }}" />
             </div>
             <!--end::Col-->
         </div>
@@ -45,7 +48,8 @@
         <!--begin::Input group-->
         <div class="fv-row mb-7">
             <label class="form-label fw-bolder text-dark fs-6">{{ __('Email') }}</label>
-            <input class="form-control form-control-lg form-control-solid" type="email" name="email" autocomplete="off" value="{{ old('email') }}"/>
+            <input class="form-control form-control-lg form-control-solid" type="email" name="email" autocomplete="off"
+                value="{{ old('email') }}" />
         </div>
         <!--end::Input group-->
 
@@ -61,9 +65,11 @@
 
                 <!--begin::Input wrapper-->
                 <div class="position-relative mb-3">
-                    <input class="form-control form-control-lg form-control-solid" type="password" name="password" autocomplete="new-password"/>
+                    <input class="form-control form-control-lg form-control-solid" type="password" name="password"
+                        autocomplete="new-password" />
 
-                    <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2" data-kt-password-meter-control="visibility">
+                    <span class="btn btn-sm btn-icon position-absolute translate-middle top-50 end-0 me-n2"
+                        data-kt-password-meter-control="visibility">
                         <i class="bi bi-eye-slash fs-2"></i>
                         <i class="bi bi-eye fs-2 d-none"></i>
                     </span>
@@ -83,26 +89,25 @@
 
             <!--begin::Hint-->
             <div class="text-muted">
-                {{ __('Gunakan minimal 8 karakter, terdapat huruf dan angka.') }}
+                {{ __('The :attribute must be at least :length characters and contain at least one number.', [
+                    'attribute' => 'Kata sandi',
+                    'length' => '8',
+                ]) }}
             </div>
             <!--end::Hint-->
         </div>
         <!--end::Input group--->
 
         <!--begin::Input group-->
-        <div class="fv-row mb-5">
-            <label class="form-label fw-bolder text-dark fs-6">{{ __('Confirm Password') }}</label>
-            <input class="form-control form-control-lg form-control-solid" type="password" name="password_confirmation" autocomplete="off"/>
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
         <div class="fv-row mb-10">
             <label class="form-check form-check-custom form-check-solid form-check-inline">
-                <input class="form-check-input" type="checkbox" name="toc" value="1"/>
+                <input class="form-check-input" type="checkbox" name="toc" value="1" />
                 <span class="form-check-label fw-bold text-gray-700 fs-6">
-                {{ __('Saya setuju dengan') }} <a href="#" class="ms-1 link-primary">{{ __('other.toc') }}</a>.
-            </span>
+                    {{ __('I agree to the :terms_of_service and :privacy_policy', [
+                        'terms_of_service' => 'syarat',
+                        'privacy_policy' => 'privasi',
+                    ]) }}
+                </span>
             </label>
         </div>
         <!--end::Input group-->
